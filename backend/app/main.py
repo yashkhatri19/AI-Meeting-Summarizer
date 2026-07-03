2d6071c924d5ecd901dad0ca27d28d94f41a9158
Yash Khatri <yashkhatri.gcsb@gmail.com>
2026-07-03 18:49:16 +0530

    Importing CORSMiddeleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="VoxBrief AI API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000","https://ai-meeting-summarizer-h93x.onrender.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ai-meeting-summarizer-guru19.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
