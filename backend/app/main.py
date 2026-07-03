243da8e2e7ad3f84bdd33b2ab30b859fd1eb4ac7
Yash Khatri <yashkhatri.gcsb@gmail.com>
2026-07-03 16:45:01 +0530

    Update origin in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="VoxBrief AI API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://ai-meeting-summarizer-h93x.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
