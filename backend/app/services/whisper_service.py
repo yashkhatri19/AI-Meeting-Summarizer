be1d5b3993548941f6220f93e1921a277c80dd44
Yash Khatri <yashkhatri.gcsb@gmail.com>
2026-07-03 13:44:21 +0530

    Checking input file

--- backend/app/services/whisper_service.py
+++ backend/app/services/whisper_service.py
@@ -1,20 +1,53 @@
 import os
-import httpx
+from dotenv import load_dotenv
+from pydub import AudioSegment
+from pydub.utils import which
 from groq import Groq
-from app.core.config import settings
+import httpx
+
+# Tell pydub exactly where your newly downloaded ffmpeg executable is located
+AudioSegment.converter = r"C:\Users\Nihal\AppData\Local\ffmpegio\ffmpeg-downloader\ffmpeg\bin\ffmpeg.exe"
+AudioSegment.ffprobe   = r"C:\Users\Nihal\AppData\Local\ffmpegio\ffmpeg-downloader\ffmpeg\bin\ffprobe.exe"
+# Load variables from .env
+load_dotenv()
 
 class WhisperService:
     def __init__(self):
-        self.client = Groq(api_key=settings.GROQ_API_KEY, http_client=httpx.Client())
+        # Setup custom client with a 10-minute timeout for large files
+        self.custom_client = httpx.Client(timeout=600.0)
+        self.client = Groq(
+            api_key=os.getenv("GROQ_API_KEY"), 
+            http_client=self.custom_client
+        )
 
-    def transcribe_audio(self, file_path: str) -> str:
-        if not os.path.exists(file_path):
-            raise FileNotFoundError("Audio file not found on server")
+    def transcribe_audio(self, input_file_path: str) -> str:
+        compressed_audio_path = "temp_compressed_audio.mp3"
         
-        with open(file_path, "rb") as audio_file:
-            translation = self.client.audio.transcriptions.create(
-                file=audio_file,
-                model="whisper-large-v3",
-                response_format="text"
-            )
-        return translation
+        try:
+            print("Compressing file to fit under Groq's 25MB limit...")
+            # Load the video/audio file
+            audio = AudioSegment.from_file(input_file_path)
+            
+            # Export to low bitrate MP3 (keeps 30 mins under ~15MB)cd
+            audio.export(compressed_audio_path, format="mp3", bitrate="64k")
+            print("Compression complete. Uploading to Groq...")
+
+            AudioSegment.converter = os.path.abspath("ffmpeg.exe")
+            AudioSegment.ffprobe = os.path.abspath("ffprobe.exe")
+            
+            # Send to Groq for translation
+            with open(compressed_audio_path, "rb") as file_to_send:
+                response = self.client.audio.transcriptions.create(
+                    file=file_to_send,
+                    model="whisper-large-v3"
+                )
+            return response.text
+
+        except Exception as e:
+            print(f"Error inside WhisperService: {str(e)}")
+            raise e
+            
+        finally:
+            # Always clean up the temporary file
+            if os.path.exists(compressed_audio_path):
+                os.remove(compressed_audio_path)
